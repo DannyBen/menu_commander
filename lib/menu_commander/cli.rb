a2c5a5b01c56040b4970724cd1c2e082d0041547
Danny Ben Shitrit <db@dannyben.com>
2020-10-29 08:17:05 +0000

    compact router call

--- lib/menu_commander/cli.rb
+++ lib/menu_commander/cli.rb
@@ -3,9 +3,7 @@ require 'mister_bin'
 module MenuCommander
   class CLI
     def self.router
-      router = MisterBin::Runner.new
-      router.route_all to: Command
-      router
+      MisterBin::Runner.new handler: Command
     end
   end
 end
